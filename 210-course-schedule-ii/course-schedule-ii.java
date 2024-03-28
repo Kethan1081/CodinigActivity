901a444c7d7f2c7d1f1cb043b8e2eef494948066
Kethan Gaddam <48917700+Kethan1081@users.noreply.github.com>
2024-03-27 21:49:00 -0700

    Time: 14 ms (11.49%) | Memory: 45.4 MB (87.20%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -0,0 +1,50 @@
+class Node {
+    int val;
+    List<Integer> adj;
+    Node(int val) {
+        this.val = val;
+        this.adj = new ArrayList();
+    }
+}
+class Solution {
+    public int[] findOrder(int n, int[][] p) {
+        Map<Integer, Node> m = new HashMap();
+        for(int i=0; i<n; ++i) {
+            m.put(i, new Node(i));
+        }
+        int[] topo = new int[n];
+        for(int[] i: p) {
+            int s = i[1];
+            int d = i[0];
+            topo[d] = topo[d] + 1;
+            Node ss = m.get(s);
+            ss.adj.add(d);
+        }
+        int[] res = new int[n];
+        int c=0;
+        Queue<Integer> q = new LinkedList();
+        for(int i=0; i<n; ++i) {
+            if(topo[i] == 0) {
+                q.offer(i);
+                topo[i] = -1;
+            }
+        }
+        while(!q.isEmpty()) {
+            int x = q.poll();
+            // topo[x] = -1;
+            res[c] = x;
+            c++;
+            for(int i: m.get(x).adj) {
+                topo[i] = topo[i]-1;
+            }
+            for(int i=0; i<n; ++i) {
+                if(topo[i] == 0) {
+                    q.offer(i);
+                    topo[i] = -1;
+                }
+            }
+        }
+        if(c == n) return res;
+        return new int[0];
+    }
+}
