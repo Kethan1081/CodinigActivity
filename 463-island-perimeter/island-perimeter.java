b6b18698642184901a4592e269370f3f1963c457
Kethan Gaddam <48917700+Kethan1081@users.noreply.github.com>
2024-04-18 14:46:46 -0700

    Time: 425 ms (5.20%) | Memory: 45.8 MB (41.51%) - LeetSync

---
+++ 463-island-perimeter/island-perimeter.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        Queue<int[]> q = new LinkedList();
+        boolean[][] vis = new boolean[m][n];
+        for(int i=0; i<m; ++i) {
+            for(int j=0; j<n; ++j) {
+                if(grid[i][j] == 1) {
+                    q.offer(new int[]{i,j});
+                    break;
+                }
+            }
+        }
+        
+        int[] d1 = {-1, 0, 1, 0};
+        int[] d2 = {0, 1, 0, -1};
+        int res=0;
+        while(!q.isEmpty()) {
+            int[] p = q.poll();
+            int r = p[0];
+            int c = p[1];
+            System.out.println(r + " - " + c);
+            if(vis[r][c]) continue;
+            vis[r][c] = true;
+            if(c-1 < 0 || grid[r][c-1] == 0) res++;
+            if(c+1 >= n || grid[r][c+1] == 0) res++;
+            if(r-1 < 0 || grid[r-1][c] == 0) res++;
+            if(r+1 >= m || grid[r+1][c] == 0) res++;
+
+            for(int i=0; i<4; ++i) {
+                int r1 = r + d1[i];
+                int c1 = c + d2[i];
+                if(r1 >=0 && r1<m && c1>=0 && c1<n && grid[r1][c1] == 1 && !vis[r1][c1]) q.offer(new int[]{r1, c1});
+            }
+        }
+        return res;
+    }
+}
